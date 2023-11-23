d9adc810fb95e487f0767dff52b959d3955ebfbd
Pacheco, King JayJay A <pachecoking38@gmail.com>
2023-11-24 07:39:59 +0800

    up

--- backend/database/hrs_db.sql
+++ backend/database/hrs_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 19, 2023 at 03:42 PM
+-- Generation Time: Nov 23, 2023 at 11:38 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -43,7 +43,8 @@ CREATE TABLE `table_product` (
 INSERT INTO `table_product` (`id`, `roomName`, `roomPrice`, `roomImg`, `roomDescription`, `roomHeart`) VALUES
 (1, 'Double Suite', '1500.00', 0x51756172746f2d5477696e2d322e6a7067, 'Two double beds into a wide windowed room.', 0),
 (6, 'Normal Room', '1050.00', 0x70726f647563742d312e6a7067, 'Perfect for a simple trip with someone special', 0),
-(8, 'Music Room', '1200.00', 0x494d475f353430322e4a5047, 'Love to sing? Then try this room!', 0);
+(8, 'Music Room', '1200.00', 0x494d475f353430322e4a5047, 'Love to sing? Then try this room!', 0),
+(125, 'wqewqe', '121321.00', '', 'qweqw', 0);
 
 -- --------------------------------------------------------
 
@@ -74,7 +75,9 @@ INSERT INTO `table_register` (`id`, `username`, `email`, `password`, `usertype`,
 (12, 'chynna', 'alcantarachynna1234@gmail.com', '$2y$10$oyVS5rvc.fPDR5I/9gqfjOM2LbNksq8QQaeNSj5LtnzabxMtNi8Om', 'student', '', '', '2023-11-16 12:28:53'),
 (14, 'Admin', 'admin@gmail.com', '$2y$10$ItWl457mTjHIq7e6tmBp1Ol9P9Y8AIDxjHLnm2gIX/IWqhCkm5wIe', 'admin', '', '', '2023-11-16 12:28:53'),
 (16, 'admin1', 'admin1@gmail.com', '$2y$10$ULVJw8JD7w3K/UVxDMrdeeL.oGkTQ4DAnuJtOuDFZb0KHYnUF23Ua', 'student', '2PlhWns382h8Uh88jS', '', '2023-11-16 12:28:53'),
-(31, 'Student', 'student@gmail.com', '$2y$10$CnUPZbFRb1VNAPaJYOo48O3kUhwqIan7gv2BNOmyc8dFj7vr.2YQ6', 'student', 'yfZk3AjVhpaOmBvtYdC0P7JU2rLxHo8R4MTqluwgEc1Is5ezQW', 'active', '2023-11-18 22:17:50');
+(40, 'Student', 'student@gmail.com', '$2y$10$f9/9O2OTcn.teHoa8aDCmOmwr5YYeflVtIbQx6e2qtSNyr/7uCs7O', 'student', '0KWHeQumot29wjOMkCSLyrXiT8l3fcA7G4bF5PNJv6zIZVExBn', 'active', '2023-11-23 06:28:19'),
+(41, 'Instructor', 'instructor@gmail.com', '$2y$10$v9/8l8ub.2A6Y9z5qR7/J.8KsfXuT/.Zi0QlM5Hz6GZCAU4lXZLyW', 'instructor', 'VgNo7U3LuimPYbSCA2X4etKrTwjB0JEs6p51fvlDOWIaqxzGkc', 'active', '2023-11-23 23:26:08'),
+(43, 'aaaaaa', 'aaaaaa@gmail.com', '$2y$10$X1ZaBMiuJQifMgAFzHy3XONl/k2/F.g/oO.TCY61Mix8DMKQJvf4G', 'student', 'd6qzBVP9jyh0sRbZn5UEWHv47cAXtfQTKLaJI8kGwxrp3mMg1e', 'active', '2023-11-24 07:36:44');
 
 --
 -- Indexes for dumped tables
@@ -100,13 +103,13 @@ ALTER TABLE `table_register`
 -- AUTO_INCREMENT for table `table_product`
 --
 ALTER TABLE `table_product`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
 
 --
 -- AUTO_INCREMENT for table `table_register`
 --
 ALTER TABLE `table_register`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
